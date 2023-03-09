3ba787e4354b8b01756a38e165f66bac316381c5
Max Yoo <devbear74@gmail.com>
2023-03-09 16:57:00 +0900

    Practice creating Binding Adapters

--- app/src/main/java/com/gandalgom/sample/databinding/MainActivity.kt
+++ app/src/main/java/com/gandalgom/sample/databinding/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.gandalgom.sample.databinding
 
-import android.content.Context
-import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 
@@ -29,28 +26,5 @@ class MainActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
-    }
-
-    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
-        return when (popularity) {
-            Popularity.NORMAL ->
-                context.theme.obtainStyledAttributes(intArrayOf(android.R.attr.colorForeground))
-                    .getColor(0, 0x000000)
-            Popularity.POPULAR ->
-                ContextCompat.getColor(context, R.color.popular)
-            Popularity.STAR ->
-                ContextCompat.getColor(context, R.color.star)
-        }
-    }
-
-    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
-        return when (popularity) {
-            Popularity.NORMAL ->
-                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
-            Popularity.POPULAR ->
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            Popularity.STAR ->
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-        }
     }
 }
--- app/src/main/java/com/gandalgom/sample/databinding/ui/BindingAdapters.kt
+++ app/src/main/java/com/gandalgom/sample/databinding/ui/BindingAdapters.kt
@@ -1,8 +1,16 @@
 package com.gandalgom.sample.databinding.ui
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
 import android.view.View
+import android.widget.ImageView
 import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+
+import com.gandalgom.sample.databinding.R
+import com.gandalgom.sample.databinding.data.Popularity
 
 @BindingAdapter("app:hideIfZero")
 fun hideIfZero(view: View, number: Int) {
@@ -18,4 +26,40 @@ fun hideIfZero(view: View, number: Int) {
 @BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
 fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
     progressBar.progress = (likes * max / 5).coerceAtMost(max)
+}
+
+@BindingAdapter("app:progressTintColor")
+fun setProgressTintColor(progressBar: ProgressBar, popularity: Popularity) {
+    val color = getAssociatedColor(popularity, progressBar.context)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+@BindingAdapter("app:popularityImage")
+fun setPopularityImage(imageView: ImageView, popularity: Popularity) {
+    val color = getAssociatedColor(popularity, imageView.context)
+    imageView.imageTintList = ColorStateList.valueOf(color)
+    imageView.setImageDrawable(getDrawablePopularity(popularity, imageView.context))
+}
+
+private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
+    return when (popularity) {
+        Popularity.NORMAL ->
+            context.theme.obtainStyledAttributes(intArrayOf(android.R.attr.colorForeground))
+                .getColor(0, 0x000000)
+        Popularity.POPULAR ->
+            ContextCompat.getColor(context, R.color.popular)
+        Popularity.STAR ->
+            ContextCompat.getColor(context, R.color.star)
+    }
+}
+
+private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
+    return when (popularity) {
+        Popularity.NORMAL ->
+            ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
+        Popularity.POPULAR ->
+            ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+        Popularity.STAR ->
+            ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
+    }
 }
