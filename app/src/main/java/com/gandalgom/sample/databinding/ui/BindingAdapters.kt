5acb44a77e436cf00eb994ce405b29fd754cbce5
Max Yoo <devbear74@gmail.com>
2023-03-09 14:53:18 +0900

    Using Binding Adapters to create custom attributes

---
+++ app/src/main/java/com/gandalgom/sample/databinding/ui/BindingAdapters.kt
@@ -0,0 +1,10 @@
+package com.gandalgom.sample.databinding.ui
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("app:hideIfZero")
+fun hideIfZero(view: View, number: Int) {
+    view.visibility = if (number == 0) View.GONE else View.VISIBLE
+}
+
