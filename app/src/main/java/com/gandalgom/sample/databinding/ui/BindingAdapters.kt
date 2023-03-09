224c3d690ba68d21cbfd12b867582062e1feea2d
Max Yoo <devbear74@gmail.com>
2023-03-09 15:01:08 +0900

    Create a Binding Adapter with multiple parameters

--- app/src/main/java/com/gandalgom/sample/databinding/ui/BindingAdapters.kt
+++ app/src/main/java/com/gandalgom/sample/databinding/ui/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package com.gandalgom.sample.databinding.ui
 
 import android.view.View
+import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("app:hideIfZero")
@@ -8,3 +9,13 @@ fun hideIfZero(view: View, number: Int) {
     view.visibility = if (number == 0) View.GONE else View.VISIBLE
 }
 
+/**
+ *  Sets the value of the progress bar so that 5 likes will fill it up.
+ *
+ *  Showcases Binding Adapters with multiple attributes. Note that this adapter is called
+ *  whenever any of the attribute changes.
+ */
+@BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
+fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
+    progressBar.progress = (likes * max / 5).coerceAtMost(max)
+}
